d94d040ee143a1045351adc855eb3a3aa88381ea
Sebas Contreras <swf.sebas@gmail.com>
2022-06-03 22:04:30 -0400

    MVC

---
+++ Veterinaria.Web/Controllers/PokemonController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Veterinaria.Web.Controllers
+{
+    public class PokemonController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Listar");
+        }
+    }
+}
---
+++ Veterinaria.Web/Models/Pokemon.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Veterinaria.Web.Models
+{
+    public class Pokemon
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public int Altura { get; set; }
+        public string Tipo { get; set; }
+        public int Peso { get; set; }
+        public bool esChainih { get; set; }
+
+        public Pokemon()
+        {
+
+        }
+    }
+}
